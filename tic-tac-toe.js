1c127d86b5171f7630d3f4001938e5ec409a8ccc
Ji You <kroyagis@gmail.com>
2016-07-24 17:34:26 -0400

    minor change

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,7 +2,8 @@ $(function(){
   var turn = 0;
   $(document).on('click', '.canvas-grid', function(){
     console.log("clicked");
-    if (turn%2 === 0 ){
+    if (turn%2 === 0){
+      console.log($(this).attr('id'));
       $(this).attr('id', 'x');
       turn++;
     }else if(turn%2 === 1){
