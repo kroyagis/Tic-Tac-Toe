e074bb1e3843d227708d968ad7d72e876d122cad
Ji You <kroyagis@gmail.com>
2016-07-26 10:01:06 -0400

    will not be able to click already clicked grid

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,11 +2,10 @@ $(function(){
   var turn = 0;
   $(document).on('click', '.canvas-grid', function(){
     console.log("clicked");
-    if (turn%2 === 0){
-      console.log($(this).attr('id'));
+    if (turn%2 === 0 && !(this.id)) {
       $(this).attr('id', 'x');
       turn++;
-    }else if(turn%2 === 1){
+    }else if(turn%2 === 1 && !(this.id)){
       $(this).attr('id', 'o');
       turn++;
     }
