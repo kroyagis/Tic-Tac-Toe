cc19321a65e9cdbf10bfd5d7e0b87d254d33c4a7
Ji You <kroyagis@gmail.com>
2016-07-28 09:59:42 -0400

    checkWinner is working for all scenarios

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,7 @@
 $(function(){
-  var winner = false;
   var turn = 0;
   $(document).on('click', '.canvas-grid', function(){
-    console.log("clicked");
+
     if (turn%2 === 0 && !(this.id)) {
       $(this).attr('id', 'x');
       turn++;
@@ -22,46 +21,53 @@ $(function(){
 
     ){
       alert($('[data-id=1]').attr('id') + " has won!");
-      winner = true;
+      resetBoard();
     }else if(
       $('[data-id=1]').attr('id') == $('[data-id=4]').attr('id')
       && $('[data-id=1]').attr('id') == $('[data-id=7]').attr('id')
       && $('[data-id=1]').attr('id')
     ){
       alert($('[data-id=1]').attr('id') + " has won!");
-      winner = true;
+      resetBoard();
     }else if(
       $('[data-id=7]').attr('id') == $('[data-id=8]').attr('id')
       && $('[data-id=7]').attr('id') == $('[data-id=9]').attr('id')
       && $('[data-id=7]').attr('id')
     ){
-      alert($('[data-id=1]').attr('id') + " has won!");
-      winner = true;
+      alert($('[data-id=7]').attr('id') + " has won!");
+      resetBoard();
     }else if(
       $('[data-id=3]').attr('id') == $('[data-id=9]').attr('id')
       && $('[data-id=3]').attr('id') == $('[data-id=6]').attr('id')
       && $('[data-id=3]').attr('id')
     ){
-      alert($('[data-id=1]').attr('id') + " has won!");
-      winner = true;
+      alert($('[data-id=3]').attr('id') + " has won!");
+      resetBoard();
     }else if(
       $('[data-id=1]').attr('id') == $('[data-id=5]').attr('id')
       && $('[data-id=1]').attr('id') == $('[data-id=9]').attr('id')
       && $('[data-id=1]').attr('id')
     ){
       alert($('[data-id=1]').attr('id') + " has won!");
-      winner = true;
+      resetBoard();
     }else if(
       $('[data-id=3]').attr('id') == $('[data-id=5]').attr('id')
       && $('[data-id=3]').attr('id') == $('[data-id=7]').attr('id')
       && $('[data-id=3]').attr('id')
     ){
-      alert($('[data-id=1]').attr('id') + " has won!");
-      winner = true;
+      alert($('[data-id=3]').attr('id') + " has won!");
+      resetBoard();
+    }else if(
+      $('[data-id=4]').attr('id') == $('[data-id=5]').attr('id')
+      && $('[data-id=4]').attr('id') == $('[data-id=6]').attr('id')
+      && $('[data-id=4]').attr('id')
+    ){
+      alert($('[data-id=4]').attr('id') + " has won!");
+      resetBoard();
     }
   }
+  var resetBoard = function(){
+    $('.canvas-grid').attr('id', '');
+    turn = 0;
+  }
 });
-
-// var resetBoard = function(){
-//   $('.canvas-grid').attr('id', '')
-// }
