40121d73e7c5977292ff19bd7a1e9a544405184d
Ji You <kroyagis@gmail.com>
2016-07-28 09:51:11 -0400

    checks winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,5 @@
 $(function(){
+  var winner = false;
   var turn = 0;
   $(document).on('click', '.canvas-grid', function(){
     console.log("clicked");
@@ -13,36 +14,54 @@ $(function(){
   });
 
   var checkWinner = function(){
+
     if(
       $('[data-id=1]').attr('id') == $('[data-id=2]').attr('id')
       && $('[data-id=1]').attr('id') == $('[data-id=3]').attr('id')
+      && $('[data-id=1]').attr('id')
+
     ){
-      console.log("Winner has been decided!");
+      alert($('[data-id=1]').attr('id') + " has won!");
+      winner = true;
     }else if(
       $('[data-id=1]').attr('id') == $('[data-id=4]').attr('id')
-      && $('[data-id=1]').attr('id') == $('[data-id=6]').attr('id')
+      && $('[data-id=1]').attr('id') == $('[data-id=7]').attr('id')
+      && $('[data-id=1]').attr('id')
     ){
-      console.log("Winner has been decided!");
+      alert($('[data-id=1]').attr('id') + " has won!");
+      winner = true;
     }else if(
       $('[data-id=7]').attr('id') == $('[data-id=8]').attr('id')
       && $('[data-id=7]').attr('id') == $('[data-id=9]').attr('id')
+      && $('[data-id=7]').attr('id')
     ){
-      console.log("Winner has been decided!");
+      alert($('[data-id=1]').attr('id') + " has won!");
+      winner = true;
     }else if(
       $('[data-id=3]').attr('id') == $('[data-id=9]').attr('id')
       && $('[data-id=3]').attr('id') == $('[data-id=6]').attr('id')
+      && $('[data-id=3]').attr('id')
     ){
-      console.log("Winner has been decided!");
+      alert($('[data-id=1]').attr('id') + " has won!");
+      winner = true;
     }else if(
       $('[data-id=1]').attr('id') == $('[data-id=5]').attr('id')
       && $('[data-id=1]').attr('id') == $('[data-id=9]').attr('id')
+      && $('[data-id=1]').attr('id')
     ){
-      console.log("Winner has been decided!");
+      alert($('[data-id=1]').attr('id') + " has won!");
+      winner = true;
     }else if(
       $('[data-id=3]').attr('id') == $('[data-id=5]').attr('id')
       && $('[data-id=3]').attr('id') == $('[data-id=7]').attr('id')
+      && $('[data-id=3]').attr('id')
     ){
-      console.log("Winner has been decided!");
+      alert($('[data-id=1]').attr('id') + " has won!");
+      winner = true;
     }
   }
 });
+
+// var resetBoard = function(){
+//   $('.canvas-grid').attr('id', '')
+// }
